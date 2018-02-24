17a31bdd8dd2550016e20e500d0fb88ad33b04fb
chabby <503589899@qq.com>
2018-02-24 17:47:25 +0800

    laradock

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,10 +3,16 @@
 namespace App\Http\Controllers;
 
 
+use Illuminate\Support\Facades\DB;
+
 class HomeController extends Controller
 {
     public function index()
     {
+
+//        $a = DB::select("select * from test");
+//        var_dump($a);exit;
+
         return view('index');
     }
 }
