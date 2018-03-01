a3ef4dd72bbee4fb7d61eefad812f1061ae58a0e
chabby <503589899@qq.com>
2018-03-01 11:29:51 +0800

    notifications

--- app/Notifications/Warning.php
+++ app/Notifications/Warning.php
@@ -7,7 +7,7 @@ use Illuminate\Notifications\Notification;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Notifications\Messages\MailMessage;
 
-class Warning extends Notification
+class Warning extends Notification implements ShouldQueue
 {
     use Queueable;
 
