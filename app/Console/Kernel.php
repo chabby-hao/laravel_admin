61183b567685b07f5efb97be2441867d1b4c303e
chabby <503589899@qq.com>
2018-03-02 11:47:03 +0800

    里程异常

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -28,7 +28,7 @@ class Kernel extends ConsoleKernel
     {
         // $schedule->command('inspire')
         //          ->hourly();
-        $schedule->command(WarningMile::class)->everyTenMinutes();
+        $schedule->command(WarningMile::class)->hourly();
     }
 
     /**
