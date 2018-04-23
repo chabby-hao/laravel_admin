7c792a84834f3272a5437124552dced5fdfebe77
chabby <503589899@qq.com>
2018-04-23 10:12:01 +0800

    sync

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -34,7 +34,7 @@ class Kernel extends ConsoleKernel
         if(env('APP_ENV') != 'local'){
             $schedule->command(WarningMile::class)->hourly();
             $schedule->command(DbSync::class)->hourly();
-            $schedule->command(DeviceCache::class)->everyThirtyMinutes();
+            $schedule->command(DeviceCache::class)->everyFifteenMinutes();
             $schedule->command(MapCache::class)->everyThirtyMinutes();
         }
     }
