a48f49adcb790edb4bb8ea73393bcc64229c4796
chabby <503589899@qq.com>
2018-05-18 17:54:48 +0800

    list 优化

--- app/Http/Controllers/Admin/DeviceController.php
+++ app/Http/Controllers/Admin/DeviceController.php
@@ -317,11 +317,12 @@ class DeviceController extends BaseController
         /** @var TDeviceCode $device */
         foreach ($deviceList as $device) {
             //$data[] = DeviceLogic::createDevice($device->imei);
-            $tmp = DeviceLogic::getDeviceFromCacheByUdid($device->qr) ?: DeviceLogic::simpleCreateDevice($device->imei);
-//            $tmp['deviceTypeName'] = $deviceTypeMap[$device->device_type];
-//            $tmp['ebikeTypeName'] = $ebikeTypeMap[$device->ebike_type_id];
-//            $tmp['brandName'] = $brandMap[$device->brand_id];
-//            $tmp['channelName'] = $channelMap[$device->channel_id];
+            $obj = DeviceLogic::getDeviceFromCacheByUdid($device->qr) ?: DeviceLogic::simpleCreateDevice($device->imei);
+            $tmp = (array)$obj;
+            $tmp['deviceTypeName'] = $deviceTypeMap[$device->device_type];
+            $tmp['ebikeTypeName'] = $ebikeTypeMap[$device->ebike_type_id];
+            $tmp['brandName'] = $brandMap[$device->brand_id];
+            $tmp['channelName'] = $channelMap[$device->channel_id];
             $data[] = $tmp;
         }
 
