f6dd40acc10077fac3d3a715561e95211ab6bbee
chabby <503589899@qq.com>
2018-05-23 14:33:09 +0800

    map

--- resources/views/admin/map/show.blade.php
+++ resources/views/admin/map/show.blade.php
@@ -815,7 +815,7 @@
     function reloadMap(res){
         var newoption = deepCopy(option, {});
         console.log(newoption);
-        newoption.title.subtext = '当前'+res.gps.length+'辆';
+        newoption.title.text = '当前'+res.gps.length+'辆';
         if(res.single){
             if(res.gps.length > 0){
                 newoption.bmap.zoom = 13;
