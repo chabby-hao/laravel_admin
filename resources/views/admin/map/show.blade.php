6eabe9d2e15e322ee09575b065b5f27cc1e9e63a
chabby <503589899@qq.com>
2018-05-29 10:51:31 +0800

    map

--- resources/views/admin/map/show.blade.php
+++ resources/views/admin/map/show.blade.php
@@ -818,7 +818,7 @@
         newoption.title.text = '当前'+res.gps.length+'辆';
         if(res.single){
             if(res.gps.length > 0){
-                newoption.bmap.zoom = 13;
+                newoption.bmap.zoom = 8;
                 newoption.bmap.center = res.gps[0].value;
                 newoption.series[0].data = res.gps;
             }
