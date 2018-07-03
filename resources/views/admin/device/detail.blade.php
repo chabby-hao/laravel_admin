f7389e8a611ede6e38fc6e621609f697c8112d57
chabby <chabby@vipcare.com>
2018-07-03 12:05:22 +0800

    详情

--- resources/views/admin/device/detail.blade.php
+++ resources/views/admin/device/detail.blade.php
@@ -167,7 +167,7 @@
                                 <td><%lastLocation.dateTime%></td>
                                 <td><%lastLocation.type%></td>
                                 <td><%#lastLocation.lng%>
-                                    <a href="{{URL::action('Admin\DeviceController@map')}}?lat=<%lastLocation.lng%>&lat=<%lastLocation.lat%>"><%lastLocation.lng%>,<%lastLocation.lat%></a>
+                                    <a href="{{URL::action('Admin\DeviceController@map')}}?lng=<%lastLocation.lng%>&lat=<%lastLocation.lat%>"><%lastLocation.lng%>,<%lastLocation.lat%></a>
                                     <%/lastLocation.lng%></td>
                                 <td><%lastLocation.address%></td>
                                 <td><%lastLocation.landmark%></td>
