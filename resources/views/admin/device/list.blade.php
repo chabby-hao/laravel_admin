9cd6228d86b9788cf6117930406302b65cffbb41
chabby <chabby@vipcare.com>
2018-07-13 16:05:35 +0800

    筛选

--- resources/views/admin/device/list.blade.php
+++ resources/views/admin/device/list.blade.php
@@ -42,6 +42,18 @@
                                     <input class="w15 margintop" type="text" id="id" name="id" value="{{Request::input('id')}}" placeholder="设备号/IMEI/IMSI/手机号">
                                     <input class="w1 margintop" type="text" name="rom" value="{{Request::input('rom')}}" placeholder="MTK版本号(rom版本号)">
                                     <input class="w1 margintop" type="text" name="mcu" value="{{Request::input('mcu')}}" placeholder="MCU版本号">
+                                    <select class="w1 margintop" name="attach">
+                                        <option value="">请选择在线状态</option>
+                                        @foreach(\App\Objects\DeviceObject::getOnlineOfflineTypeMap() as $k => $v)
+                                            <option @if(is_numeric(Request::input('attach')) && Request::input('attach') == $k) selected @endif value="{{$k}}">{{$v}}</option>
+                                        @endforeach
+                                    </select>
+                                    <select class="w1 margintop" name="active">
+                                        <option value="">请选择是否激活</option>
+                                        @foreach(\App\Objects\DeviceObject::getActiveTypeMap() as $k => $v)
+                                            <option @if(is_numeric(Request::input('active')) && Request::input('active') == $k) selected @endif value="{{$k}}">{{$v}}</option>
+                                        @endforeach
+                                    </select>
                                     <br>
                                     <select class="w1 margintop" name="device_type">
                                         <option value="">请选择型号</option>
