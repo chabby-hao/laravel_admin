b2741654a2be42e6407b429ddfb1cf7c07cc004e
chabby <chabby@vipcare.com>
2018-07-24 11:57:33 +0800

    渠道秘钥

--- app/Logics/ChannelKeylogic.php
+++ app/Logics/ChannelKeylogic.php
@@ -9,16 +9,19 @@ use App\Models\BiChannelSecret;
 class ChannelKeylogic extends BaseLogic
 {
 
-    const REDIS_HASH_KEY = 'channel_secret';
+    const REDIS_HASH_CHANNEL_TO_CONFIG = 'channel_to_config';
+    const REDIS_HASH_CHANNEL_TO_SECRET = 'channel_to_secret';
 
     public static function refreshAllConfig()
     {
         $datas = BiChannelSecret::select(['secret','channel_id','channel_name as channel','push_url'])->get()->toArray();
-        $datas = Helper::transToKeyToArray($datas, 'channel_id');
-        $datas = array_map(function($row){
+        $channelConfig = Helper::transToKeyToArray($datas, 'channel_id');
+        $channelConfig = array_map(function($row){
             return json_encode($row);
-        }, $datas);
-        return RedisLogic::hmSet(self::REDIS_HASH_KEY, $datas);
+        }, $channelConfig);
+        $channelSecret = Helper::transToKeyValueArray($datas, 'channel', 'secret');
+        RedisLogic::hmSet(self::REDIS_HASH_CHANNEL_TO_CONFIG, $channelConfig);
+        RedisLogic::hmSet(self::REDIS_HASH_CHANNEL_TO_SECRET, $channelSecret);
     }
 
 
