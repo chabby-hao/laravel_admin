0e14ded71c0c8de442d8d2f200dbcb7d2a3a0911
chabby <chabby@vipcare.com>
2018-07-25 18:22:03 +0800

    config

--- app/Logics/ChannelKeylogic.php
+++ app/Logics/ChannelKeylogic.php
@@ -15,9 +15,12 @@ class ChannelKeylogic extends BaseLogic
 
     public static function refreshAllConfig()
     {
-        $datas = BiChannelSecret::select(['secret','channel_id','channel_name as channel','push_url'])->get()->toArray();
+        $datas = BiChannelSecret::select(['secret','channel_id','channel_name as channel','push_url','push_types'])->get()->toArray();
         $channelConfig = Helper::transToKeyToArray($datas, 'channel_id');
         $channelConfig = array_map(function($row){
+            if($row['push_types']){
+                $row['push_types'] = explode(',', $row['push_types']);
+            }
             return json_encode($row);
         }, $channelConfig);
         $channelSecret = Helper::transToKeyValueArray($datas, 'channel', 'secret');
