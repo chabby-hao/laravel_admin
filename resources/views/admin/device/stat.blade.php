0f387e2f131a6eca454582899896d1836a1cde2d
eme <eme@localhost>
2018-08-06 19:12:01 +0800

    part 1

--- resources/views/admin/device/stat.blade.php
+++ resources/views/admin/device/stat.blade.php
@@ -2,31 +2,37 @@
 @section('content')
 
     <div class="container-fluid">
-    		<div class="row car_info">
-		  	<div class="span4">
-		  		<h3 class="car_jrhy">750</h3>
-		  		<p>今日活跃（人）</p>
-		  	</div>
-		  	<div class="span4">
-		  		<h3 class="car_cxcs">19,123</h3>
-		  		<p>出行次数（次）</p>
-		  	</div>
-		  	<div class="span4">
-		  		<h3 class="car_cxpc">1.9</h3>
-		  		<p>出行频次（次）</p>
-		  	</div>
-		  	<div class="span4">
-		  		<h3 class="car_cxjl">26,907,855</h3>
-		  		<p>出行距离（km）</p>
-		  	</div>
+		<div class="row-fluid car_info">
+			<div class="span3">
+				<h3>
+					<span class="car_jrhy">750</span>
+					<i class="arr_down"></i>
+				</h3>
+				<p>今日活跃(人)</p>
+			</div>
+			<div class="span3">
+				<h3>
+					<span class="car_cxcs">19,123</span>
+					<i class="arr_up"></i>
+				</h3>
+				<p>出行次数(次)</p>
+			</div>
+			<div class="span3">
+				<h3>
+					<span class="car_cxpc">1.9</span>
+					<i class="arr_down"></i>
+				</h3>
+				<p>出行频次(次)</p>
+			</div>
+			<div class="span3">
+				<h3>
+					<span class="car_cxjl">907,855</span>
+					<i class="arr_up"></i>
+				</h3>
+				<p>出行距离(km)</p>
+			</div>
 		</div>
-    		
-    		
-    		
-    		
-    		
 
-
-    </div>
+	</div>
 
 @endsection
