7435dfff173ba4f608fe4d914f2ccd7407147567
eme <eme@localhost>
2018-08-06 15:37:52 +0800

    1

--- resources/views/admin/device/stat.blade.php
+++ resources/views/admin/device/stat.blade.php
@@ -2,6 +2,29 @@
 @section('content')
 
     <div class="container-fluid">
+    		<div class="row-fluid car_info">
+		  	<div class="span4">
+		  		<h3 class="car_jrhy">750</h3>
+		  		<p>今日活跃（人）</p>
+		  	</div>
+		  	<div class="span4">
+		  		<h3 class="car_cxcs">19,123</h3>
+		  		<p>出行次数（次）</p>
+		  	</div>
+		  	<div class="span4">
+		  		<h3 class="car_cxpc">1.9</h3>
+		  		<p>出行频次（次）</p>
+		  	</div>
+		  	<div class="span4">
+		  		<h3 class="car_cxjl">26,907,855</h3>
+		  		<p>出行距离（km）</p>
+		  	</div>
+		</div>
+    		
+    		
+    		
+    		
+    		
 
 
     </div>
