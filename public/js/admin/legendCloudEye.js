519c378797ecdf0d19d4700fa1c6d73bb4db6598
Eme <eme@Legend.local>
2018-08-15 09:38:38 +0800

    车辆页面数据完成

--- public/js/admin/legendCloudEye.js
+++ public/js/admin/legendCloudEye.js
@@ -1,116 +1,133 @@
-// 地图
-	var myCharts5 = echarts.init(document.getElementById('myMap'));
-	var option5 = {
-	    title : {
-	        x:'center'
-	    },	  
-	    tooltip: {
+$(function(){
+    //乘法精度
+    function accMul(arg1,arg2)  
+    {  
+         var m=0,s1=arg1.toString(),s2=arg2.toString();  
+         try{m+=s1.split(".")[1].length}catch(e){}  
+         try{m+=s2.split(".")[1].length}catch(e){}  
+         return Number(s1.replace(".",""))*Number(s2.replace(".",""))/Math.pow(10,m)  
+    };
+    
+	$.ajax({ 
+       type:"get",
+       url:"http://bidev.vipcare.com/admin/device/stat?a=1",
+       async:true,
+       dataType:'json',
+       cache: false,
+       data:{},
+       success:function(data){
+           return data;
+       },
+       error:function(){
+           // console.log(msg);
+       }
+	});
+
+    console.log(data)
+    // $('.car_jrhy').html(data.dailyActive);
+    // $('.car_cxcs').html(data.travelTimes);
+    // $('.car_cxpc').html(data.travelFrequency);
+    // $('.car_cxjl').html(data.tripDistance);
+
+    //数字滚动
+    var options = {
+      useEasing : true, 
+      useGrouping : true, 
+      separator : ',', 
+      decimal : '.', 
+      prefix : '', 
+      suffix : '' 
+    };
+    var p2num1 = new CountUp("p2-num1", 0, data.dailyActive, 0, 2.5, options);
+    var p2num2 = new CountUp("p2-num2", 0, data.travelTimes, 0, 2.5, options);
+    var p2num3 = new CountUp("p2-num3", 0, data.travelFrequency, 1, 2.5, options);
+    var p2num4 = new CountUp("p2-num4", 0, data.tripDistance, 1, 2.5, options);
+    p2num1.start();
+    p2num2.start();
+    p2num3.start();
+    p2num4.start();
+
+    // 地图
+    var mapData=data.activeGeographicalDistribution;
+    var myCharts5 = echarts.init(document.getElementById('myMap'));  
+    var option5 = {
+        title : {
+            x:'center'
+        },    
+        tooltip: {
             trigger: 'item',
             showDelay: 0,
-            transitionDuration: 0.2,
+            transitionDuration: 0.2,           
             formatter: function (params) {
                 var value = (params.value + '').split('.');
                 value = value[0].replace(/(\d{1,3})(?=(?:\d{3})+(?!\d))/g, '$1,');
-                return params.seriesName + '<br/>' + params.name + ': ' + value;
+                var zb=params.data.zb;
+                return params.name + '<br/>' + '活跃车辆: ' + value+ '<br/>' +'占比: '+zb*100+'%';
             }
         },
-		dataRange: {
-			orient:'horizontal',
+        dataRange: {
+            orient:'horizontal',
             x: 'left',
             y: 'bottom',
             splitList: [
-                {start: 3000, end: 5000,label:'高',color: '#84b0d6'},
-                {start: 2000, end: 3000, label: '中高',color: '#84b0d6'},
-                {start: 1000, end: 2000, label: '中',color: '#5c758b'},
-                {start: 100, end: 1000, label: '中低', color: '#4b5d6e'},
-                {end: 100,label:'低',color: '#43515f'}
+                {start: 700, end: 1000,label:'高',color: '#84b0d6'},
+                {start: 400, end: 700, label: '中高',color: '#7498b8'},
+                {start: 100, end: 400, label: '中',color: '#5c758b'},
+                {start: 50, end: 100, label: '中低', color: '#4b5d6e'},
+                {end: 50,label:'低',color: '#43515f'}
             ],
-            color: ['#43515f', '#4b5d6e', '#5c758b', '#84b0d6', '#84b0d6'],
+            color: ['#43515f', '#4b5d6e', '#5c758b', '#7498b8', '#84b0d6'],
             textStyle: {
-	            color: '#bfbfbf',         // 值域文字颜色
-	        },
+                color: '#bfbfbf',         // 值域文字颜色
+            },
         },
-	    showLegendSymbol : true, 
-	    roamController: {
-	        show: true,
-	        x: 'right',
-	        mapTypeControl: {
-	            'china': true
-	        }
-	    },
-	    series : [
-	        {
-	            type: 'map',
-	            mapType: 'china',
-	            roam: false,
-	            itemStyle: {
-	                normal: {
-	                    borderColor:'#89b5db',
-	                    color: '#ffce1f',
-	                    areaColor:'#4c5459',
-	                    label: {
-	                        show: false
-	                    }
-	                },
-	                emphasis:{
-	                	label:{
-	                		show:false
-	                	}
-	                },	             
-	            },
-	            data:[	    
-	            	{
-	                    name:"南海诸岛",value:0,
-	                    itemStyle:{
-	                        normal:{opacity:0,label:{show:false}}
-	                    }
-	                },         
-	                {name: '北京',value: 1000},
-                    {name: '天津',value: 1200},
-                    {name: '上海',value: 980},
-                    {name: '重庆',value: 500},
-                    {name: '河北',value: 300},
-                    {name: '河南',value: 4500},
-                    {name: '云南',value: 5},
-                    {name: '辽宁',value: 305},
-                    {name: '黑龙江',value: 320},
-                    {name: '湖南',value: 200},
-                    {name: '安徽',value: 1500},
-                    {name: '山东',value: 1100},
-                    {name: '新疆',value: 600},
-                    {name: '江苏',value: 700},
-                    {name: '浙江',value: 111},
-                    {name: '江西',value: 98},
-                    {name: '湖北',value: 222},
-                    {name: '广西',value: 333},
-                    {name: '甘肃',value: 555},
-                    {name: '山西',value: 150},
-                    {name: '内蒙古',value: 1003},
-                    {name: '陕西',value:1400},
-                    {name: '吉林',value: 800},
-                    {name: '福建',value: 700},
-                    {name: '贵州',value: 500},
-                    {name: '广东',value: 450},
-                    {name: '青海',value: 852},
-                    {name: '西藏',value: 144},
-                    {name: '四川',value: 564},
-                    {name: '宁夏',value: 2000},
-                    {name: '海南',value: 654},
-                    {name: '台湾',value:545},
-                    {name: '香港',value: 111},
-                    {name: '澳门',value: 555}
-	            ]
-	        }
-	    ]
-	};
-	myCharts5.setOption(option5);
+        showLegendSymbol : true, 
+        roamController: {
+            show: true,
+            x: 'right',
+            mapTypeControl: {
+                'china': true
+            }
+        },
+        series : [
+            {
+                type: 'map',
+                mapType: 'china',
+                roam: false,
+                itemStyle: {
+                    normal: {
+                        borderColor:'#89b5db',
+                        color: '#ffce1f',
+                        areaColor:'#4c5459',
+                        label: {
+                            show: false
+                        }
+                    },
+                    emphasis:{
+                        label:{
+                            show:false
+                        }
+                    },               
+                },
+                data:mapData                
+            }
+        ]
+    };
+    myCharts5.setOption(option5);
 
-	//车型分布柱状图
-	var myCharts = echarts.init(document.getElementById('tb_carType'));
-    var option = {      		        
+    //车型分布柱状图
+    var zhuData=data.vehicleDistribution;
+    var zhuXd=[];   //x轴name 
+    var zhuYd=[];   //value值
+    $.each(zhuData, function(i,result) {
+        zhuXd.push(result.name);
+        zhuYd.push(result.value);
+    });
+    var myCharts = echarts.init(document.getElementById('tb_carType'));
+    var option = {                      
         grid: {
-        	width:'110%',
-        	top:'0%',
+            width:'110%',
+            top:'5%',
             left: '-10%',
             right: '5%',
             bottom: '0%',
@@ -120,16 +137,20 @@
             trigger: 'item',
             showDelay: 0,
             transitionDuration: 0.2,
-            
+            formatter: function (params) { 
+                var zb=zhuData[params.dataIndex].zb;
+                zb=accMul(zb,100);
+                return '品牌: ' + zhuData[params.dataIndex].brand + '<br/>' + '车型: ' + zhuData[params.dataIndex].name+ '<br/>' +'占比: '+zb+'%';              
+            }
         },
         xAxis: {
-            data: ["雅迪","小鸟","小牛","爱玛","小刀","享骑"],
+            data: zhuXd,
             axisLabel:{
                 interval:0,
                 textStyle: {
                     color: '#7a7a7a',
-                    fontSize:16
-                }
+                    fontSize:12,
+                },
             },
             axisLine: {
                 lineStyle: {
@@ -146,41 +167,41 @@
             }
         },
         yAxis: {
-        	max: 5000,
-            splitNumber:10,					
-        	axisLabel : {
+            // max: 5000,
+            splitNumber:10,                 
+            axisLabel : {
                 show:false
-            },		               
+            },                     
             axisTick:{
                 show:false
             },
             axisLine:{
-            	show:false
+                show:false
             },
-			splitLine:{
-				show:true,
-				lineStyle: {   
-		            color: '#e7e7e7',
-		            width: 1,
-		            type: 'solid'
-		        }
-			},
+            splitLine:{
+                show:true,
+                lineStyle: {   
+                    color: '#e7e7e7',
+                    width: 1,
+                    type: 'solid'
+                }
+            },
         },
         series: [
             {
                 name: '',
                 type: 'bar',
-                data: ['1000', '4500', '3000', '1500', '2100', '3400'],
+                data: zhuYd,
                 barWidth : 30,
                 itemStyle: {
-                	normal: {
-                		label : {
+                    normal: {
+                        label : {
                             show : true,
                             position:'top',
                             textStyle: {
-		                        fontSize: 14,
-		                        color :'#6a6a6a'		                        
-		                    }
+                                fontSize: 14,
+                                color :'#6a6a6a'                                
+                            }
                         },                       
                         color: function(params) {
                             // build a color map as your need.
@@ -188,49 +209,62 @@
                               '#e3ffff','#c5ffff','#b3edff','#a9e0ff','#8cb9e2'                             
                             ];
                             if(params.value>0&&params.value<=1000){
-                            	return colorList[0];
-                            }else if(params.value>1000&&params.value<=2000){
-                            	return colorList[1];
-                            }else if(params.value>2000&&params.value<=3000){
-                            	return colorList[2];
-                            }else if(params.value>3000&&params.value<=4000){
-                            	return colorList[3];
-                            }else if(params.value>4000&&params.value<=5000){
-                            	return colorList[4];
+                                return colorList[0];
+                            }else if(params.value>1000&&params.value<=3000){
+                                return colorList[1];
+                            }else if(params.value>3000&&params.value<=5000){
+                                return colorList[2];
+                            }else if(params.value>5000&&params.value<=7000){
+                                return colorList[3];
+                            }else if(params.value>7000){
+                                return colorList[4];
                             }                            
                         } 
 
-                   }		                        
+                   }                                
                 },
                 emphasis: {
-	                shadowBlur: 10,
-	                shadowOffsetX: 0,
-	                shadowColor: 'rgba(0, 0, 0, 0.8)'
-	            }
+                    shadowBlur: 10,
+                    shadowOffsetX: 0,
+                    shadowColor: 'rgba(0, 0, 0, 0.8)'
+                }
             }            
         ],
         
     };
     myCharts.setOption(option);
 
-
-
-	// 七日活跃折线图
-	var myCharts2 = echarts.init(document.getElementById('tb_sevenDay'));
+    // 七日活跃折线图
+    var zheData=data.activeCurve;
+    console.log(zheData);
+    var zheXd=[];   //x轴name 
+    var zheYd=[];   //value值
+    $.each(zheData, function(i,result) {
+        zheXd.push(result.name);
+        zheYd.push(result.value);
+    });
+    var myCharts2 = echarts.init(document.getElementById('tb_sevenDay'));
     var option1 = {
-        tooltip: {
-            trigger: 'axis',
-        },       
         grid: {
-        	width:'110%',
-        	top:'0%',
+            width:'110%',
+            top:'5%',
             left: '-10%',
             right: '5%',
             bottom: '0%',
             containLabel: true
-        },            
+        },
+        tooltip: {
+            trigger: 'axis',
+            showDelay: 0,
+            transitionDuration: 0.2,
+            formatter: function (params) {               
+                var zb=zheData[params[0].dataIndex].zb;
+                zb=accMul(zb,100);
+                return '活跃车辆: ' + zheData[params[0].dataIndex].value+ '<br/>' +'活跃率: '+zb+'%';              
+            }
+        },                          
         xAxis: {
-            data: ["08.01","08.02","08.03","08.04","08.05","08.06"],
+            data: zheXd,
             axisLabel:{
                 interval:0,
                 textStyle: {
@@ -253,25 +287,25 @@
             }
         },
         yAxis: {
-        	max: 5000,
-            splitNumber:10,					
-        	axisLabel : {
+            
+            splitNumber:10,                 
+            axisLabel : {
                 show:false
-            },		               
+            },                     
             axisTick:{
                 show:false
             },
             axisLine:{
-            	show:false
+                show:false
             },
-			splitLine:{
-				show:true,
-				lineStyle: {   
-		            color: '#e7e7e7',
-		            width: 1,
-		            type: 'solid'
-		        }
-			},
+            splitLine:{
+                show:true,
+                lineStyle: {   
+                    color: '#e7e7e7',
+                    width: 1,
+                    type: 'solid'
+                }
+            },
         },
         series: [           
             {
@@ -279,10 +313,10 @@
                 type:'line',
                 symbol:false,
                 stack: '总量',
-                data: ['1000', '4500', '3000', '1500', '2100', '3400'],
+                data: zheYd,
                 itemStyle: {
                     normal: {
-                        color: '#5b9bd5',		                            
+                        color: '#5b9bd5',                                   
                         lineStyle:{
                             color: '#5b9bd5'
                         }
@@ -296,67 +330,109 @@
                 position: 'top',
                 textStyle: {
                     fontSize: 14,
-		            color :'#6a6a6a'                        
-                },		                 
+                    color :'#6a6a6a'                        
+                },                       
             }
         }
     };
     myCharts2.setOption(option1);
 
+
     // 出行次数饼图
+
+    var bingData=data.tripFrequencyDistribution;
     var myCharts4 = echarts.init(document.getElementById('tb_trip'));
     var i=0;
-	var colors=['#6bc3dc','#ee5b3e','#b5b5b5','#faa046','#589fcc'];
+    var colors=['#6bc3dc','#ee5b3e','#b5b5b5','#faa046','#589fcc'];
     var option4 = {
-	    tooltip : {
-	        trigger: 'item',
-	        formatter: "{b} : {c}%"
-	    },
-	    legend: {
-	        x : 'center',
-	        y : 'bottom',
-	        data:['1次','2次','3次','4次','4次以上'],
-	        selectedMode:false,	      
-	    },	    
-	    calculable : true,
-	    series : [  
-	        {
-	            name:'',
-	            type:'pie',
-	            radius : [0, 110],
-	            center : ['50%', '40%'],
-	            roseType : 'area',
-	            itemStyle:{
-	            	normal:{
-	            		color:function(){
+        tooltip : {
+            trigger: 'item',
+            formatter: function (params) { 
+                var zb=bingData[params.dataIndex].zb;
+                zb=accMul(zb,100);
+                return '出行' + bingData[params.dataIndex].name  +'用户占比: '+zb+'%';              
+            }
+        },
+        legend: {
+            x : 'center',
+            y : 'bottom',
+            data:['1次','2次','3次','4次','4次以上'],
+            selectedMode:false,       
+        },      
+        calculable : true,
+        series : [  
+            {
+                name:'',
+                type:'pie',
+                radius : '55%',
+                center : ['50%', '40%'],
+                roseType : 'area',
+                itemStyle:{
+                    normal:{
+                        color:function(){
                             return colors[i++];
                         },
                         label: {
-                            formatter: '{c}%',
+                            formatter: function(params){
+                                var zb=bingData[params.dataIndex].zb;
+                                zb=accMul(zb,100);
+                                return zb+'%'; 
+                            },
                             textStyle: {
-		                        fontSize:16,                       
-		                    }
+                                fontSize:16,                       
+                            }
                         },
-	            	}
-	            },
-	           	lableLine: {
-	                normal: {
-	                    show: false
-	                },
-	                emphasis: {
-	                    show: true
-	                }
-	            },
-	            data:[
-	                {value:10, name:'1次'},
-	                {value:5, name:'2次'},
-	                {value:15, name:'3次'},
-	                {value:25, name:'4次'},
-	                {value:20, name:'4次以上'},	                
-	            ]
-	        }
-	    ]
-	};
+                    }
+                },
+                lableLine: {
+                    normal: {
+                        show: false
+                    },
+                    emphasis: {
+                        show: true
+                    }
+                },
+                data:bingData
+            }
+        ]
+    };
     myCharts4.setOption(option4);
+    
+    window.onresize = function(){
+        setTimeout(myCharts5.resize(),100);
+        setTimeout(myCharts.resize(),100);
+        setTimeout(myCharts2.resize(),100);
+        setTimeout(myCharts4.resize(),100);
+    };
+
+
+
+
+
+})
+
+
+
+
+
+
+
+
+
+
+	
+
+
+	
+
+
+    
+
+
+
+
+
+
+
 
 
--- resources/views/admin/device/stat.blade.php
+++ resources/views/admin/device/stat.blade.php
@@ -5,39 +5,43 @@
 		<div class="row-fluid car_info">
 			<div class="span3">
 				<h3>
-					<span class="car_jrhy">750</span>
+					<span class="car_jrhy" id="p2-num1"></span>
 					<i class="arr_down"></i>
 				</h3>
 				<p>今日活跃(人)</p>
+				
 			</div>
 			<div class="span3">
 				<h3>
-					<span class="car_cxcs">19,123</span>
+					<span class="car_cxcs" id="p2-num2"></span>
 					<i class="arr_up"></i>
 				</h3>
 				<p>出行次数(次)</p>
+				
 			</div>
 			<div class="span3">
-				<h3>
-					<span class="car_cxpc">1.9</span>
+				<h3> 
+					<span class="car_cxpc" id="p2-num3"></span>
 					<i class="arr_down"></i>
 				</h3>
 				<p>出行频次(次)</p>
+				
 			</div>
 			<div class="span3">
 				<h3>
-					<span class="car_cxjl">907,855</span>
+					<span class="car_cxjl" id="p2-num4"></span>
 					<i class="arr_up"></i>
 				</h3>
 				<p>出行距离(km)</p>
+				
 			</div>
 		</div>
 		<!-- 地图 -->
 		<div class="map_box">
 			<p class="map_title_lt">活跃车辆地理分布</p>					
        		<div class="map-box">
-    				<div id="myMap"></div>    		
-    			</div>				     
+    			<div id="myMap"></div>    		
+    		</div>				     
 	    	<p class="map_title_lb">中国,东八区GTM+8</p>
 		</div>
 		<!-- 图表 -->
@@ -45,14 +49,14 @@
 			<div class="span4">
 				<p class="tb_title">车型分布</p>
 				<div>
-					<p class="tb_title_top"><span class="">5000</span><span>单位：辆</span></p>
+					<p class="tb_title_top"><span class=""></span><span>单位：辆</span></p>
 	    			<div id="tb_carType"></div>
 	    		</div>
 			</div>
 			<div class="span4">
 				<p class="tb_title">七日活跃曲线图</p>
 				<div class="">
-					<p class="tb_title_top"><span>5000</span><span>2018</span></p>
+					<p class="tb_title_top"><span></span><span>2018</span></p>
 	    			<div id="tb_sevenDay"></div>
 	    		</div>
 			</div>
@@ -60,8 +64,8 @@
 				<p class="tb_title">出行次数分布</p>
 				<div class="">
 					<p class="tb_title_top"><span></span><span>单位：次</span></p>
-	    			<div id="tb_trip"></div>
-	    		</div>
+	    				<div id="tb_trip"></div>
+	    			</div>
 			</div>					
 		</div>
 		<!-- 信息展示 -->
@@ -89,4 +93,5 @@
 	<script type="text/javascript" src="{{ asset('js/admin/echarts.min.js') }}"></script>
 	<script type="text/javascript" src="{{ asset('js/admin/map.js') }}"></script>
 	<script type="text/javascript" src="{{ asset('js/admin/legendCloudEye.js') }}"></script>
+	<script type="text/javascript" src="{{ asset('js/admin//countUp.js') }}"></script>
 @endsection
