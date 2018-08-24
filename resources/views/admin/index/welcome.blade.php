3907b054d44446c7cb89c06ed7b28b7356e3a237
Eme <eme@Legend.local>
2018-08-24 17:36:13 +0800

    welcome 修改

--- resources/views/admin/index/welcome.blade.php
+++ resources/views/admin/index/welcome.blade.php
@@ -1,5 +1,11 @@
 @extends('admin.layout')
 @section('content')
+<style>
+	/*8.24新增 应对整体优化*/
+	@media(max-width: 480px) {
+		#myiframe{width: 300%;}
+	}
+</style>
     <iframe name="myiframe" id="myiframe" src="{{URL::action('Admin\MapController@show')}}" width="100%" style="min-height: 300px" frameborder="0" scrolling="no">
         <p>你的浏览器不支持iframe标签</p>
     </iframe>
