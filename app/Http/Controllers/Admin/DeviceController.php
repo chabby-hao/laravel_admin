ca994734e4a6c21bf088dab9e8f1e50004b70523
chabby <chabby@vipcare.com>
2018-08-28 16:44:42 +0800

    流量页面

--- app/Http/Controllers/Admin/DeviceController.php
+++ app/Http/Controllers/Admin/DeviceController.php
@@ -859,7 +859,7 @@ class DeviceController extends BaseController
     public function cardList()
     {
         $r = BiCardLiangxun::join('care.t_device_code', function (JoinClause $join){
-            $join->on('substr(t_device_code.imsi,2) = bi_card_liangxun.imsi');
+            $join->on(DB::raw('substr(t_device_code.imsi,2) = bi_card_liangxun.imsi'));
         })->limit(100)->get()->toArray();
         dd($r);
     }
