c966949289a7a993a1acaf2d3e0671204e7a20a9
chabby <chabby@vipcare.com>
2018-08-28 15:06:52 +0800

    流量同步

--- app/Console/Commands/UpiotSync.php
+++ app/Console/Commands/UpiotSync.php
@@ -3,28 +3,10 @@
 namespace App\Console\Commands;
 
 
-use App\Libs\Helper;
 use App\Libs\UpiotApi;
-use App\Logics\DeviceLogic;
-use App\Logics\LocationLogic;
-use App\Models\BiActiveCityDevice;
-use App\Models\BiActiveDevice;
-use App\Models\BiBrand;
 use App\Models\BiCardDatum;
 use App\Models\BiCardLiangxun;
-use App\Models\BiChannel;
-use App\Models\BiEbikeType;
-use App\Models\BiProductType;
-use App\Models\BiProvince;
-use App\Models\TDevice;
-use App\Models\TDeviceCategory;
-use App\Models\TDeviceCategoryDicNew;
-use App\Models\TDeviceCode;
-use App\Objects\DeviceObject;
 use Carbon\Carbon;
-use Illuminate\Support\Facades\Cache;
-use Illuminate\Support\Facades\DB;
-use Illuminate\Support\Facades\Log;
 
 class UpiotSync extends BaseCommand
 {
