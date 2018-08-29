09901062dd141baee955831404353a353535d009
chabby <chabby@vipcare.com>
2018-08-29 12:00:57 +0800

    流量页面

--- app/Logics/DeviceLogic.php
+++ app/Logics/DeviceLogic.php
@@ -318,7 +318,6 @@ class DeviceLogic extends BaseLogic
     {
         if($card = BiCardLiangxun::whereMsisdn($msisdn)->first()){
             var_dump($card->imsi);
-            $imsi = ;
             if($deviceCode = TDeviceCode::whereImsi('9' . $card->imsi)->first()){
                 var_dump($deviceCode->qr);
                 return $deviceCode->qr;
