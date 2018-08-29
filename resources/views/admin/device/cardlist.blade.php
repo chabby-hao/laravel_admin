53bf78c4f841cef7254a3f97e7619327d640324e
chabby <chabby@vipcare.com>
2018-08-29 17:16:09 +0800

    流量页面

--- resources/views/admin/device/cardlist.blade.php
+++ resources/views/admin/device/cardlist.blade.php
@@ -82,7 +82,6 @@
                 <div class="widget-box">
                     <div class="widget-title"><span class="icon"><i class="icon-th"></i></span>
                         <h5>列表</h5>
-                        <span class="top-pager">{!! $page_nav !!}</span>
                     </div>
                     <div class="widget-content nopadding">
                         <table class="table table-bordered data-table">
