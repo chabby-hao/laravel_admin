52088f04dd82569bb12be62c2a0ebb44004b569c
chabby <chabby@vipcare.com>
2018-08-29 10:15:25 +0800

    流量页面

--- app/Http/Controllers/Admin/DeviceController.php
+++ app/Http/Controllers/Admin/DeviceController.php
@@ -867,7 +867,7 @@ class DeviceController extends BaseController
 
         $paginate = $model
             ->select(['bi_card_liangxun.*','imei','qr as udid','channel_id'])
-            ->orderByDesc('data_usage')->simplePaginate();
+            ->orderByDesc('data_usage')->limit(20);
 
         dd($paginate);
 
