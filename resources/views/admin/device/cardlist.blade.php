ed0b10dcd1ed7bec21a24cb2035e81a53fb534a1
chabby <chabby@vipcare.com>
2018-08-29 12:05:08 +0800

    流量页面

--- resources/views/admin/device/cardlist.blade.php
+++ resources/views/admin/device/cardlist.blade.php
@@ -10,42 +10,33 @@
                 <div class="widget-box">
                     <div class="widget-title"><span class="icon"> <i class="icon-search"></i> </span>
                         <h5>筛选查询</h5>
-                        @if(Auth::user()->can('device/importCity'))
-                            <form id="myform" method="post" enctype="multipart/form-data" action="{{URL::action('Admin\DeviceController@importCity')}}" class="form-search">
-                                <span class="pull-right"><input type="file" name="myfile"/></span>
-                                <span class="pull-right"><a class="btn btn-info" href="{{asset('demo/importcity.xlsx')}}">导入示例</a></span>
-                            </form>
-                        @endif
                     </div>
                     <div class="widget-content">
                         <form action="" method="get" class="form-search search_form">
-                            @if(Request::input('status'))
-                                <input type="hidden" name="status" value="{{Request::input('status')}}">
-                            @endif
                             <div class="control-group">
                                 <div class="inline-block w10">
                                     <input class="w15 margintop" type="text" id="id" name="id" value="{{Request::input('id')}}" placeholder="设备号/IMEI/IMSI/手机号/卡号">
                                     <input class="w1 margintop" type="text" name="rom" value="{{Request::input('rom')}}" placeholder="MTK版本号(rom版本号)">
                                     <input class="w1 margintop" type="text" name="mcu" value="{{Request::input('mcu')}}" placeholder="MCU版本号">
-                                    <select class="w1 margintop" name="attach">
-                                        <option value="">请选择在线状态</option>
-                                        @foreach(\App\Objects\DeviceObject::getOnlineOfflineTypeMap() as $k => $v)
-                                            <option @if(is_numeric(Request::input('attach')) && Request::input('attach') == $k) selected @endif value="{{$k}}">{{$v}}</option>
-                                        @endforeach
-                                    </select>
-                                    <select class="w1 margintop" name="active">
-                                        <option value="">请选择是否激活</option>
-                                        @foreach(\App\Objects\DeviceObject::getActiveTypeMap() as $k => $v)
-                                            <option @if(is_numeric(Request::input('active')) && Request::input('active') == $k) selected @endif value="{{$k}}">{{$v}}</option>
-                                        @endforeach
-                                    </select>
-                                    <br/>
-                                    <select class="w1 margintop" name="device_type">
-                                        <option value="">请选择型号</option>
-                                        @foreach(\App\Models\BiDeviceType::getNameMap() as $k => $v)
-                                            <option @if(Request::input('device_type') == $k) selected @endif value="{{$k}}">{{$v}}</option>
-                                        @endforeach
-                                    </select>
+                                    {{--<select class="w1 margintop" name="attach">--}}
+                                        {{--<option value="">请选择在线状态</option>--}}
+                                        {{--@foreach(\App\Objects\DeviceObject::getOnlineOfflineTypeMap() as $k => $v)--}}
+                                            {{--<option @if(is_numeric(Request::input('attach')) && Request::input('attach') == $k) selected @endif value="{{$k}}">{{$v}}</option>--}}
+                                        {{--@endforeach--}}
+                                    {{--</select>--}}
+                                    {{--<select class="w1 margintop" name="active">--}}
+                                        {{--<option value="">请选择是否激活</option>--}}
+                                        {{--@foreach(\App\Objects\DeviceObject::getActiveTypeMap() as $k => $v)--}}
+                                            {{--<option @if(is_numeric(Request::input('active')) && Request::input('active') == $k) selected @endif value="{{$k}}">{{$v}}</option>--}}
+                                        {{--@endforeach--}}
+                                    {{--</select>--}}
+                                    {{--<br/>--}}
+                                    {{--<select class="w1 margintop" name="device_type">--}}
+                                        {{--<option value="">请选择型号</option>--}}
+                                        {{--@foreach(\App\Models\BiDeviceType::getNameMap() as $k => $v)--}}
+                                            {{--<option @if(Request::input('device_type') == $k) selected @endif value="{{$k}}">{{$v}}</option>--}}
+                                        {{--@endforeach--}}
+                                    {{--</select>--}}
                                     @if(Auth::user()->user_type == \App\Models\BiUser::USER_TYPE_ALL)
                                         <select class="w1 margintop" name="channel_id">
                                             <option value="">请选择渠道</option>
@@ -53,28 +44,28 @@
                                                 <option @if(Request::input('channel_id') == $k) selected @endif value="{{$k}}">{{$v}}</option>
                                             @endforeach
                                         </select>
-                                        <select class="w1 margintop" name="customer_id">
-                                            <option value="">
-                                                请选择客户
-                                            </option>
-                                        </select>
-                                        <select class="w1 margintop" name="scene_id">
-                                            <option value="">
-                                                请选择场景
-                                            </option>
-                                        </select>
-                                        <br/>
-                                        <select class="w1 margintop" name="brand_id">
-                                            <option value="">请选择品牌</option>
-                                            @foreach(\App\Models\BiBrand::getBrandMap() as $k => $v)
-                                                <option @if(Request::input('brand_id') == $k) selected @endif value="{{$k}}">{{$v}}</option>
-                                            @endforeach
-                                        </select>
-                                        <select class="w1 margintop" name="ebike_type_id">
-                                            <option value="">
-                                                请选择车型
-                                            </option>
-                                        </select>
+                                        {{--<select class="w1 margintop" name="customer_id">--}}
+                                            {{--<option value="">--}}
+                                                {{--请选择客户--}}
+                                            {{--</option>--}}
+                                        {{--</select>--}}
+                                        {{--<select class="w1 margintop" name="scene_id">--}}
+                                            {{--<option value="">--}}
+                                                {{--请选择场景--}}
+                                            {{--</option>--}}
+                                        {{--</select>--}}
+                                        {{--<br/>--}}
+                                        {{--<select class="w1 margintop" name="brand_id">--}}
+                                            {{--<option value="">请选择品牌</option>--}}
+                                            {{--@foreach(\App\Models\BiBrand::getBrandMap() as $k => $v)--}}
+                                                {{--<option @if(Request::input('brand_id') == $k) selected @endif value="{{$k}}">{{$v}}</option>--}}
+                                            {{--@endforeach--}}
+                                        {{--</select>--}}
+                                        {{--<select class="w1 margintop" name="ebike_type_id">--}}
+                                            {{--<option value="">--}}
+                                                {{--请选择车型--}}
+                                            {{--</option>--}}
+                                        {{--</select>--}}
                                     @endif
 
                                     <input type="submit" class="btn btn-success margintop search" value="查询">
