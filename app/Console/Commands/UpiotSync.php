aa45d102b8575cdd2b1bed2ca54537408c3cb0cd
chabby <chabby@vipcare.com>
2018-08-30 10:48:55 +0800

    流量页面

--- app/Console/Commands/UpiotSync.php
+++ app/Console/Commands/UpiotSync.php
@@ -7,9 +7,12 @@ use App\Libs\UpiotApi;
 use App\Models\BiCardDatum;
 use App\Models\BiCardLiangxun;
 use Carbon\Carbon;
+use Illuminate\Support\Facades\Cache;
 
 class UpiotSync extends BaseCommand
 {
+
+    const CACHE_KEY_PRE = 'pre_data_usage:';
 
     protected $signature = 'upiot:sync';
     protected $description = '流量统计脚本';
@@ -23,13 +26,13 @@ class UpiotSync extends BaseCommand
     public function handle()
     {
 
-        $this->cardListSync();
+        //$this->cardListSync();
 
         $this->cardDataUsageSync();
 
     }
 
-    private function cardListSync()
+    protected function cardListSync()
     {
         $upiot = new UpiotApi();
         $upiot->cardListSync(function($data){
@@ -43,12 +46,24 @@ class UpiotSync extends BaseCommand
         });
     }
 
-    private function cardDataUsageSync()
+    /**
+     * 流量日明细同步，因为没有日明细接口，所以，用昨日的月流量-前日的月流量=昨日的日流量
+     */
+    protected function cardDataUsageSync()
+    {
+        $predate = Carbon::today()->subDays(2)->format('Ymd');
+        $date = Carbon::yesterday()->format('Ymd');
+        $this->cardDataUsage($predate);
+        $this->cardDataUsage($date);
+
+    }
+
+    protected function cardDataUsage($date)
     {
         $model = BiCardLiangxun::where([]);
 
         $upiotApi = new UpiotApi();
-        $date = Carbon::yesterday()->format('Ymd');
+        //$date = Carbon::yesterday()->format('Ymd');
 
         $msisdns = [];
         $this->batchSearch($model, function (BiCardLiangxun $row) use ($upiotApi, &$msisdns, $date) {
@@ -73,30 +88,52 @@ class UpiotSync extends BaseCommand
         $upiotApi->clearPromise();
     }
 
-    private function dataUsage(UpiotApi $upiotApi, $msisdns, $date)
+    protected function twoDayAgoDataUsage(UpiotApi $upiotApi, $msisdns, $date)
     {
         //异步获取
-        $upiotApi->getDataUsageAsync($msisdns, $date, function($data) use ($date){
+        $upiotApi->getDataUsageAsync($msisdns, $date, function ($data) use ($date) {
+            echo json_encode($data) . "\n";
+            if ($data['data']) {
+                foreach ($data['data'] as $item) {
+                    Cache::store('redis')->put($this->getKey($item['msisdn'], $date), $item['data_usage'], 60 * 24);
+                }
+            }
+        });
+    }
+
+    protected function getKey($msisdn, $date)
+    {
+        return self::CACHE_KEY_PRE . $date . $msisdn;
+    }
+
+    protected function dataUsage(UpiotApi $upiotApi, $msisdns, $date)
+    {
+        //异步获取
+        $upiotApi->getDataUsageAsync($msisdns, $date, function ($data) use ($date) {
             $this->dataUsageDbUpdate($data, $date);
         });
     }
 
-    private function dataUsageDbUpdate($data, $date)
+    protected function dataUsageDbUpdate($data, $date)
     {
+        $predate = Carbon::parse($date)->subDay(1)->format('Ymd');
         echo json_encode($data) . "\n";
         if($data['data']){
             foreach ($data['data'] as $item){
+                $preUsage = Cache::store('redis')->get($this->getKey($item['msisdn'], $predate)) ?: 0;
+                $diff = $item['data_usage'] - $preUsage;
+                $dataUsage = $diff > 0 ? $diff : 0;
                 BiCardLiangxun::updateOrCreate([
                     'msisdn'=>$item['msisdn'],
                 ],[
-                    'current_date_usage'=>$item['data_usage'],
+                    'current_date_usage'=>$dataUsage,
                 ]);
 
                 BiCardDatum::updateOrCreate([
                     'msisdn'=>$item['msisdn'],
                     'date'=>$date,
                 ],[
-                    'data_usage'=>$item['data_usage'],
+                    'data_usage'=>$dataUsage,
                 ]);
 
             }
