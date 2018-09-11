3f9c3e076124408cef376a0365294a8e31e240dd
chabby <chabby@vipcare.com>
2018-09-11 16:28:40 +0800

    userDeviceClear

--- resources/views/admin/tool/userdevicedel.blade.php
+++ resources/views/admin/tool/userdevicedel.blade.php
@@ -19,9 +19,9 @@
                             </div>
 
                             <div class="form-actions">
-                                <span class="help-block">本次操作会清除设备所关联的所有用户</span>
                                 <button type="button" id="mysubmit" class="btn btn-success">提交</button>
                             </div>
+                            <span class="help-block">本次操作会清除设备所关联的所有用户</span>
                         </form>
                     </div>
                 </div>
