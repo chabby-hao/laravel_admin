47f7b4f2fe7d09b6cd82e4ba301ef39f11c89bbc
chabby <chabby@vipcare.com>
2018-09-18 20:10:22 +0800

    安骑物联-电池统计脚本

--- app/Console/Commands/StatBattery.php
+++ app/Console/Commands/StatBattery.php
@@ -108,6 +108,7 @@ class StatDevice extends BaseCommand
         $charging = 0;//充电中
         $using = 0;//使用中
         $this->batchSearch($model, function (TDeviceCode $deviceCode) use (&$low, &$high, &$lowPower, &$charging, &$using){
+            DeviceLogic::clear();
             $imei = $deviceCode->imei;
             $key = 'last_battery:' . $imei;
             if($data = RedisLogic::getDevDataByImei($imei)){
@@ -132,7 +133,6 @@ class StatDevice extends BaseCommand
                     ++$using;
                 }
             }
-            DeviceLogic::clear();
 
         });
 
@@ -166,6 +166,8 @@ class StatDevice extends BaseCommand
             ],
         ];
 
+        $rs;
+
 
 
     }
@@ -176,21 +178,111 @@ class StatDevice extends BaseCommand
     private function remainElectricity($where, $id, $keyPre)
     {
         $model = TDeviceCode::where($where);
-        $bat5 = 0;//小于5
-        $bat525 = 0;//5-25
+        $bat05 = 0;//小于5
+        $bat0525 = 0;//5-25
         $bat2550 = 0;
         $bat5075 = 0;
         $bat75 = 0;//大于75
-        $this->batchSearch($model, function (TDeviceCode $deviceCode) use (&$bat5, &$high, &$lowPower, &$charging, &$using){
-
+        $this->batchSearch($model, function (TDeviceCode $deviceCode) use (&$bat05, &$bat0525, &$bat2550, &$bat5075, &$bat75) {
+            $imei = $deviceCode->imei;
+            if (RedisLogic::getDevDataByImei($imei)) {
+                $battery = DeviceLogic::getBattery($imei);
+                if ($battery > 75) {
+                    ++$bat75;
+                } elseif ($battery > 50) {
+                    ++$bat5075;
+                } elseif ($battery > 25) {
+                    ++$bat0525;
+                } else {
+                    ++$bat05;
+                }
+            }
+            DeviceLogic::clear();
         });
+
+        $total = $bat05 + $bat0525 + $bat5075 + $bat75 + $bat2550;
+
+        $rs = [
+            [
+                'name' => '<5%',
+                'value' => $bat05,
+                'zb' => round($bat05/$total),
+            ],
+            [
+                'name' => '5%-25',
+                'value' => $bat0525,
+                'zb' => round($bat0525/$total),
+            ],
+            [
+                'name' => '25%-50%',
+                'value' => $bat2550,
+                'zb' => round($bat2550/$total),
+            ],
+            [
+                'name' => '50%-75%',
+                'value' => $bat5075,
+                'zb' => round($bat5075/$total),
+            ],
+            [
+                'name' => '>75%',
+                'value' => $bat75,
+                'zb' => round($bat75/$total),
+            ],
+        ];
+
+        $rs;
     }
 
     /**
      * 电池使用时间分布
      */
-    private function batteryUsingTimeDistribution()
+    private function batteryUsingTimeDistribution($where, $id, $keyPre)
     {
+        $month1 = Carbon::now()->subMonth()->getTimestamp();
+        $coutMonth1 = TDeviceCode::where($where)->where('active','>=', $month1)->count();//1个月内
+
+        $month6 = Carbon::now()->subMonths(6)->getTimestamp();
+        $coutMonth16 = TDeviceCode::where($where)->whereBetween('active',[$month6, $month1])->count();//1-6个月内
+
+        $month12 = Carbon::now()->subMonths(12)->getTimestamp();
+        $coutMonth612 = TDeviceCode::where($where)->whereBetween('active',[$month12, $month6])->count();//6-12个月内
+
+        $month24 = Carbon::now()->subMonths(24)->getTimestamp();
+        $coutMonth1224 = TDeviceCode::where($where)->whereBetween('active',[$month24, $month12])->count();//12-24个月内
+
+        $coutMonth24 = TDeviceCode::where($where)->whereBetween('active',[1, $month24])->count();//24个月以上
+
+        $total = $coutMonth1 + $coutMonth16 + $coutMonth612 + $coutMonth1224 + $coutMonth24;
+
+        $rs = [
+            [
+                'name'=>'1个月以内',
+                'value'=>$coutMonth1,
+                'zb'=>ceil($coutMonth1/$total),
+            ],
+            [
+                'name'=>'1-6个月',
+                'value'=>$coutMonth16,
+                'zb'=>ceil($coutMonth16/$total),
+            ],
+            [
+                'name'=>'6-12个月',
+                'value'=>$coutMonth612,
+                'zb'=>ceil($coutMonth612/$total),
+            ],
+            [
+                'name'=>'12-24个月',
+                'value'=>$coutMonth1224,
+                'zb'=>ceil($coutMonth1224/$total),
+            ],
+            [
+                'name'=>'24个月以上',
+                'value'=>$coutMonth24,
+                'zb'=>ceil($coutMonth24/$total),
+            ],
+        ];
+
+        $rs;
 
     }
 
