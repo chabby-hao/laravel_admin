958e1343a1846a4af816489f7205847d4a4329f9
chabby <chabby@vipcare.com>
2018-10-17 10:23:26 +0800

    config set

--- resources/views/admin/tool/configset.blade.php
+++ resources/views/admin/tool/configset.blade.php
@@ -31,23 +31,25 @@
                                 </table>
                             </div>
                             <div class="span6">
-                                <form class="form-search" id="myform" method="post" enctype="multipart/form-data">
-                                    <div class="control-group">
-                                        <div class="controls controls-row">
-
-                                            <div class="inline-block">
-                                                <label>上传附件</label>
-                                                <input class="file-uploading" name="myfile" type="file"/>
-                                                {{--<span class="help-block">点击下载模板示例: <a href="">示例文件</a></span>--}}
-                                            </div>
-
-                                            <div class="inline-block">
-                                                <span class="help-block">点击下载附件示例: <a href="{{asset('demo/factdemo.xlsx')}}">示例文件</a></span>
-                                            </div>
-
-                                        </div>
-                                    </div>
-                                </form>
+                                <table class="">
+                                    <tbody>
+                                    <tr>
+                                        <td><strong>料号：{{$data->part_number}}</strong></td>
+                                    </tr>
+                                    <tr>
+                                        <td><strong>出货单号：{{$data->ship_no}}</strong></td>
+                                    </tr>
+                                    <tr>
+                                        <td><strong>设备型号：{{$data->device_type_name}}</strong></td>
+                                    </tr>
+                                    <tr>
+                                        <td><strong>要求出货时间：{{$data->delivery_date}}</strong></td>
+                                    </tr>
+                                    <tr>
+                                        <td><strong>出货数量：{{$data->delivery_quantity}}</strong></td>
+                                    </tr>
+                                    </tbody>
+                                </table>
                             </div>
                         </div>
                         <div class="row-fluid">
@@ -62,64 +64,6 @@
                                 </div>
                             </div>
                         </div>
-                    </div>
-                </div>
-            </div>
-            <div class="span6">
-                <div class="widget-box">
-                    <div class="widget-content">
-                        <form id="myform" method="post" class="form-horizontal">
-
-                            <div class="control-group single">
-                                <label class="control-label"><span class="text-error">*</span>设备码 :</label>
-                                <div class="controls">
-                                    <input name="id" value="" type="text" class="span11"/>
-                                    <span class="help-block">可输入：设备码/IMEI/IMSI/张飞ID/得威Id</span>
-                                </div>
-                            </div>
-
-                            <div class="control-group single">
-                                <label class="control-label"><span class="text-error">*</span>命令编号 :</label>
-                                <div class="controls">
-                                    <input name="cmd" value="" type="text" class="span11"/>
-                                    <span class="help-block">例如：24003</span>
-                                </div>
-                            </div>
-
-
-                            <div class="form-actions">
-                                <button type="button" id="mysubmit" class="btn btn-success">提交</button>
-                            </div>
-                        </form>
-                    </div>
-                </div>
-            </div>
-            <div class="span6">
-                <div class="widget-box">
-                    <div class="widget-content">
-                        <form id="myform" method="post" class="form-horizontal">
-
-                            <div class="control-group single">
-                                <label class="control-label"><span class="text-error">*</span>设备码 :</label>
-                                <div class="controls">
-                                    <input name="id" value="" type="text" class="span11"/>
-                                    <span class="help-block">可输入：设备码/IMEI/IMSI/张飞ID/得威Id</span>
-                                </div>
-                            </div>
-
-                            <div class="control-group single">
-                                <label class="control-label"><span class="text-error">*</span>命令编号 :</label>
-                                <div class="controls">
-                                    <input name="cmd" value="" type="text" class="span11"/>
-                                    <span class="help-block">例如：24003</span>
-                                </div>
-                            </div>
-
-
-                            <div class="form-actions">
-                                <button type="button" id="mysubmit" class="btn btn-success">提交</button>
-                            </div>
-                        </form>
                     </div>
                 </div>
             </div>
