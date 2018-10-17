006e421a39ca0408164c11d3c88bf376988e835d
chabby <chabby@vipcare.com>
2018-10-17 11:20:32 +0800

    config set

--- resources/views/admin/tool/configset.blade.php
+++ resources/views/admin/tool/configset.blade.php
@@ -13,25 +13,22 @@
                                 <table class="">
                                     <tbody>
                                     <tr>
-                                        <td><strong>料号：{{$data->part_number}}</strong></td>
+                                        <td>料号：{{$data->part_number}}</td>
                                     </tr>
                                     <tr>
-                                        <td><strong>出货单号：{{$data->ship_no}}</strong></td>
+                                        <td>料号：{{$data->part_number}}</td>
                                     </tr>
                                     <tr>
-                                        <td><strong>设备型号：{{$data->device_type_name}}</strong></td>
+                                        <td>料号：{{$data->part_number}}</td>
                                     </tr>
                                     <tr>
-                                        <td><strong>要求出货时间：{{$data->delivery_date}}</strong></td>
-                                    </tr>
-                                    <tr>
-                                        <td><strong>出货数量：{{$data->delivery_quantity}}</strong></td>
+                                        <td>料号：{{$data->part_number}}</td>
                                     </tr>
                                     </tbody>
                                 </table>
                             </div>
                             <div class="span6">
-                                <h6>DeviceSendCgf</h6>
+                                <h5>DeviceSendCgf</h5>
                                 <table class="">
                                     <tbody>
                                     <tr>
